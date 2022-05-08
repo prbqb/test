8cfe108134b191adcf78809c9b60ec4db11f66e6
平凡之路 <18138864531@sina.cn>
2022-05-08 22:36:32 +0800

    commit new doing

--- serverB/src/main/java/com/apl/inner/sys/ServerB.java
+++ serverB/src/main/java/com/apl/inner/sys/ServerB.java
@@ -19,6 +19,7 @@ public class ServerB {
     public static void main(String[] args) {
         System.out.println("i am user2");
         SpringApplication.run(ServerB.class, args);
+        System.out.println("i am new doing ");
     }
 
 
